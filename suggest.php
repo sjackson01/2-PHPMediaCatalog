6a7f847adbefd99e20e888573cd3c6757782371c
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-05 12:46:03 -0600

    Add: Name form

--- suggest.php
+++ suggest.php
@@ -5,7 +5,15 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a Media Item</h1>
+    <div class="wrapper">
+        <h1>Suggest a Media Item</h1>
+        <p>If you think there is something I&rsquo;m missing, 
+           let me know! Complete the form to send me an e-mail. </p>
+           <!--Add a 'name' form-->
+           <form method="post">
+            <label for="name">Name<input type="text" id="name" name="name"/></label>
+           </form>
+    </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
