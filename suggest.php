1ecfba3b726cb3619ae52b30bf3b68152ffd7b3a
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-05 13:34:02 -0600

    Expand: Form suggest.php

--- suggest.php
+++ suggest.php
@@ -9,9 +9,27 @@ include("inc/header.php"); ?>
         <h1>Suggest a Media Item</h1>
         <p>If you think there is something I&rsquo;m missing, 
            let me know! Complete the form to send me an e-mail. </p>
-           <!--Add a 'name' form-->
+           <!--Add form-->
            <form method="post">
-            <label for="name">Name<input type="text" id="name" name="name"/></label>
+           <table>
+             <tr> 
+                <th><label for="name">Name</label></th>
+                <td><input type="text" id="name" name="name"/></td>
+             </tr>
+             <tr> 
+                <th><label for="email">EMail</label></th>
+                <td><input type="text" id="email" name="email"/></td>
+             </tr>
+             <tr> 
+                <th><label for="email">Suggest Item Details</label></th>
+                <td><textarea type="text" id="email" name="email"></textarea></td>
+             </tr>
+             <tr> 
+                <th><label for="email">Suggest Item Details</label></th>
+                <td><textarea type="text" id="email" name="email"></textarea></td>
+             </tr>
+           </table>
+                <input type="submit" value="Send" />
            </form>
     </div>
 </div>
