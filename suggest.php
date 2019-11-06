f6db20e6802637b2401ab4236b1b69012df7d0a1
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-06 10:02:10 -0600

    Move: Process.php code to suggest.php

--- suggest.php
+++ suggest.php
@@ -1,8 +1,30 @@
 <?php 
+//Using value keys save the values from the $_POST array to single variables
+//Gather all variables from the form
+$name = $_POST["name"];
+$email = $_POST["email"];
+$details = $_POST["details"]; 
+/*
+$_POST output 
+array(3) { 
+    ["name"]=> string(14) "Steven Jackson" 
+    ["email"]=> string(15) "email@email.com" 
+    ["suggest"]=> string(15) "Some suggestion" }
+*/
+//Add name and email to email body
+$email_body = "";
+$email_body .= "Name " . $name . "\n"; 
+$email_body .= "Email " . $email . "\n";
+$email_body .= "Details " . $details . "\n";
+
+header("location:thanks.php");
+
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
-include("inc/header.php"); ?>
+include("inc/header.php"); 
+
+?>
 
 <div class="section page">
     <div class="wrapper">
@@ -10,7 +32,7 @@ include("inc/header.php"); ?>
         <p>If you think there is something I&rsquo;m missing, 
            let me know! Complete the form to send me an e-mail. </p>
            <!--Add form-->
-           <form method="post" action="process.php">
+           <form method="post" action="suggest.php">
            <table>
              <tr> 
                 <th><label for="name">Name</label></th>
