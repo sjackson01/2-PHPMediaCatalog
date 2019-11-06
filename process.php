577a00c5f0bfa2c36dba3a2f5060cbf90917101b
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-05 22:32:00 -0600

    Fix: HTML tag closure

--- process.php
+++ process.php
@@ -25,7 +25,7 @@ $section = null;
 include("inc/header.php");
 ?>
 
-<div class="section page"
+<div class="section page">
     <h1> Thank you </h1>
     <p> Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>
 </div>
